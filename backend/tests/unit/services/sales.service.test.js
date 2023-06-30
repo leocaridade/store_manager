7211a7b658fdaa9b33a6109700c9636e94a10fdf
leocaridade <leocaridade@hotmail.com>
2023-06-30 15:25:10 -0400

    Create unit tests for function createSale on service layer

--- backend/tests/unit/services/sales.service.test.js
+++ backend/tests/unit/services/sales.service.test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const { salesModel } = require('../../../src/models');
 const { salesService } = require('../../../src/services');
-const { allSales, allSalesById } = require('../sales.mocks');
+const { allSales, allSalesById, salesIdFromModel } = require('../sales.mocks');
 
 describe('Sales Service Tests', function () {
   it('Should be able to find all sales successfully', async function () {
@@ -27,6 +27,96 @@ describe('Sales Service Tests', function () {
     const response = await salesService.findById(inputData);
     expect(response.status).to.equal('SUCCESSFUL');
     expect(response.data).to.deep.equal(allSalesById);
+  });
+
+  it('Should be able to create a sales successfully', async function () {
+    sinon.stub(salesModel, 'insertSales').resolves(salesIdFromModel);
+
+    const inputData = [
+      {
+        productId: 1,
+        quantity: 1,
+      },
+      {
+        productId: 2,
+        quantity: 5,
+      },
+    ];
+    const response = await salesService.createSale(inputData);
+
+    expect(response).to.be.an('object');
+    expect(response.status).to.equal('CREATED');
+    expect(response.data).to.deep.equal({ id: salesIdFromModel, itemsSold: inputData });
+  });
+
+  it('Should fail to create a sale without column "productId"', async function () {
+    const inputData = [
+      {
+        productId: 1,
+        quantity: 1,
+      },
+      {
+        quantity: 5,
+      },
+    ];
+    const response = await salesService.createSale(inputData);
+
+    expect(response).to.be.an('object');
+    expect(response.status).to.equal('BAD_REQUEST');
+    expect(response.data).to.deep.equal({ message: '"productId" is required' });
+  });
+
+  it('Should fail to create a sale without column "quantity"', async function () {
+    const inputData = [
+      {
+        productId: 1,
+      },
+      {
+        productId: 2,
+        quantity: 5,
+      },
+    ];
+    const response = await salesService.createSale(inputData);
+
+    expect(response).to.be.an('object');
+    expect(response.status).to.equal('BAD_REQUEST');
+    expect(response.data).to.deep.equal({ message: '"quantity" is required' });
+  });
+
+  it('Should fail to create a sale with a quantity less than or equal to 0', async function () {
+    const inputData = [
+      {
+        productId: 1,
+        quantity: 0,
+      },
+      {
+        productId: 2,
+        quantity: 5,
+      },
+    ];
+    const response = await salesService.createSale(inputData);
+
+    expect(response).to.be.an('object');
+    expect(response.status).to.equal('UNPROCESSABLE_ENTITY');
+    expect(response.data).to.deep.equal({ message: '"quantity" must be greater than or equal to 1' });
+  });
+
+  it('Should fail to create a sale with a non-existent productId', async function () {
+    const inputData = [
+      {
+        productId: 5,
+        quantity: 1,
+      },
+      {
+        productId: 2,
+        quantity: 5,
+      },
+    ];
+    const response = await salesService.createSale(inputData);
+
+    expect(response).to.be.an('object');
+    expect(response.status).to.equal('NOT_FOUND');
+    expect(response.data).to.deep.equal({ message: 'Product not found' });
   });
 
   afterEach(function () {
